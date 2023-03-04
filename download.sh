425272153337b7222dee7b49112fdb19ff1ac540
Ameero11 <126987238+Ameero11@users.noreply.github.com>
2023-03-04 23:08:25 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NzgzNjg2MzB9fX1dfQ__&Signature=v6gMP7Zj1MHK2K1yNoQupC-7u0Y6N-~-fJM--7Gox5ng2ADfULf8EyFpjNSS2iBz4wSxm960uIADWMLclXb-TK5poA6cLbX2MiqcoJ4TXtISNG2Vy3MB0IxqR43xDwTugsZF-3lMfMvl3BDhAsXxoOHzkI188tyOGR37Zu~QTCY9Xl5X-thX3wS3AFa~~2FVnAjfKhEJs9EYukKrecT-y04QC~WN5qUp5kT6svxmLHRjcMoveLqZak-6bpgZQibAkjr-U8uue9IqBA8uY4d6eKtC8PO~GTd7YuWXsKOJXx9F2g2tqwqZsMPmVKd3LNQquko88a3fZNfwdJJgvWOlyw__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
 MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
 TARGET_FOLDER=""             # where all files should end up
 
