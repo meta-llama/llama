6e2a0b57708cf490b7c0653e235db359ef4c149b
Istvan Szukacs <istvan@datadeft.eu>
2023-03-11 12:36:01 +0100

    Starting to add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+# This software may be used and distributed according to the
+# terms of the GNU General Public License version 3.
 
 from setuptools import setup, find_packages
 
