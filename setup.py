55ab44b7645834231dd5b898c97c714180b56b4b
Istvan Szukacs <istvan@datadeft.eu>
2023-03-11 13:41:48 +0100

    Torch is not happy with pyproject.toml

--- setup.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the
-# terms of the GNU General Public License version 3.
-
-from setuptools import setup, find_packages
-
-setup(name="llama", version="0.0.0", packages=find_packages())
