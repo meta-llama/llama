c2d8c435c4e17869ee36c88d16a35683dfac11ca
Bowen Bao <bowbao@microsoft.com>
2023-07-14 14:54:07 -0700

    Remove unnecessary model attribute assignment on 'freqs_cis'

--- llama/model.py
+++ llama/model.py
@@ -223,8 +223,8 @@ class Transformer(nn.Module):
     def forward(self, tokens: torch.Tensor, start_pos: int):
         _bsz, seqlen = tokens.shape
         h = self.tok_embeddings(tokens)
-        self.freqs_cis = self.freqs_cis.to(h.device)
-        freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
+        freqs_cis = self.freqs_cis.to(h.device)
+        freqs_cis = freqs_cis[start_pos : start_pos + seqlen]
 
         mask = None
         if seqlen > 1:
