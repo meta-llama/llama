3805003a3e40e3f7a0c42a37ad572bc84b4248a2
Vinny Meller <vinnymeller@gmail.com>
2023-07-20 05:04:31 -0500

    Update download.sh to not use hardcoded bash path

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
