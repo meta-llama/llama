396db5933f3f61ea512a9dae9f5860c74550ceaf
Krishnaraj Bhat <krrishnarraj@gmail.com>
2023-07-20 13:00:04 +0530

    [download.sh] make downloads resumable for large files
    wget --continue can resume downloads if the script was interrupted

--- download.sh
+++ download.sh
@@ -49,7 +49,7 @@ do
 
     for s in $(seq -f "0%g" 0 ${SHARD})
     do
-        wget ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/consolidated.${s}.pth"
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/consolidated.${s}.pth"
     done
 
     wget ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/params.json"
