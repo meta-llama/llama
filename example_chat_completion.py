7f6487a51e8903f5bc0856a6e9a31eace559ff8c
Dove. J. Kim <kimhippowork@gmail.com>
2023-07-27 08:23:24 +0000

    ['23.07.27][FIX] llama 출력 결과가 제대로 디코딩 되지 않는 부분 수정
    확실히 GPU 하나만 사용할 때 바로 OOM이 뜸.CPU로만 사용하면 너무 오래 걸림.

--- example_chat_completion.py
+++ example_chat_completion.py
@@ -25,34 +25,9 @@ def main(
     )
 
     dialogs = [
-        [{"role": "user", "content": "what is the recipe of mayonnaise?"}],
-        [
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-            {
-                "role": "assistant",
-                "content": """\
-Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:
-
-1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.
-2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.
-3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.
-
-These are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world.""",
-            },
-            {"role": "user", "content": "What is so great about #1?"},
-        ],
-        [
-            {"role": "system", "content": "Always answer with Haiku"},
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": "Always answer with emojis",
-            },
-            {"role": "user", "content": "How to go from Beijing to NY?"},
-        ],
+        [{"role": "user", "content": "Do you know Osaka?"}],
     ]
+    
     results = generator.chat_completion(
         dialogs,  # type: ignore
         max_gen_len=max_gen_len,
