62f242dc8055b239fea134d140a225be55175f01
George Tamer <gtamer2@gmail.com>
2023-12-12 22:13:36 -0500

    Changes

--- inference_benchmark.py
+++ inference_benchmark.py
@@ -1,10 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
 import time
-import cotracker.models.build_cotracker
-from cotracker.datasets.tap_vid_datasets import TapVidDataset
-import os 
-from cotracker.datasets.utils import collate_fn
 from datasets import load_dataset
 
 ### Setup ###
@@ -20,6 +16,29 @@ torch.cuda.manual_seed(SEED)
 DEVICE_CUDA = 'cuda'
 DEVICE_CPU = 'cpu'
 
+from llama import Llama
+from typing import List
+
+prompts: List[str] = [
+    # For these prompts, the expected answer is the natural continuation of the prompt
+    "I believe the meaning of life is",
+    "Simply put, the theory of relativity states that ",
+    """A brief message congratulating the team on the launch:
+
+    Hi everyone,
+    
+    I just """,
+    # Few shot prompt (providing a few examples before asking model to complete more);
+    """Translate English to French:
+    
+    sea otter => loutre de mer
+    peppermint => menthe poivrée
+    plush girafe => girafe peluche
+    cheese =>""",
+]
+
+def get_device():
+    return torch.device(DEVICE_CUDA if torch.cuda.is_available() else DEVICE_CPU)
 
 def get_data_loader(num_workers=1):
     dataset = load_dataset("HuggingFaceH4/no_robots")
@@ -27,38 +46,43 @@ def get_data_loader(num_workers=1):
         dataset,
         batch_size=BATCH_SIZE,
         shuffle=False,
-        num_workers=num_workers,
-        collate_fn=collate_fn,
+        num_workers=num_workers
     )
     return dataloader
 
 
-def get_model(checkpoint_path=CHECKPOINT_S4_W12):
-    return cotracker.models.build_cotracker.build_cotracker(checkpoint_path)
+def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+    )
+    return generator
 
 
-def run_inference(dataloader, model, cuda=True):
+def run_benchmark(dataloader, model):
     load_time_per_batch = torch.zeros(BATCH_COUNT)
     inference_time_per_batch = torch.zeros(BATCH_COUNT)
     total_time_per_batch = torch.zeros(BATCH_COUNT)
     
-    device = DEVICE_CUDA if cuda else DEVICE_CPU
-    print("Working on device: {}".format(device))
+    device = get_device()
     model.to(device)
+    print("Working on device: {}".format(device))
+    
     
     for batch_idx in range(BATCH_COUNT):
-        print("Starting BATCHs {} of {}".format(batch_idx + 1, BATCH_COUNT))
-        (output, load_time, train_time), batch_time = measure_runtime(run_batch_inference,
+        print("Starting BATCH {} of {}".format(batch_idx + 1, BATCH_COUNT))
+        (output, load_time, inference_time), batch_time = measure_runtime(run_batch_inference,
                                                               dataloader,
-                                                              model,
-                                                              cuda)
+                                                              model)
         load_time_per_batch[batch_idx] = load_time
-        inference_time_per_batch[batch_idx] = train_time
+        inference_time_per_batch[batch_idx] = inference_time
         total_time_per_batch[batch_idx] = batch_time
 
         print("Finished Batch {} of {}".format(batch_idx + 1, BATCH_COUNT))
         print("Batch load time: {}".format(load_time))
-        print("Batch inference time: {}".format(train_time))
+        print("Batch inference time: {}".format(inference_time))
         print("Batch total time: {}".format(batch_time))
     return model, load_time_per_batch, inference_time_per_batch, total_time_per_batch
 
@@ -71,19 +95,38 @@ def measure_runtime(func, *func_args):
     return result, elapsed
 
 
-def run_batch_inference(dataloader, model, cuda=True):
-    (x, y), load_time = measure_runtime(
+def run_batch_inference(dataloader, model):
+    x, load_time = measure_runtime(
         __get_next_batch, dataloader)
 
-    if cuda:
-        x = x.to(DEVICE_CUDA)
-        y = y.to(DEVICE_CUDA)
+    device = get_device()
+    x = x.to(device)
+    # y = y.to(device)
 
-    output, train_time = measure_runtime(
+    output, inference_time = measure_runtime(
+        inference,
         model,
         x)
     
-    return output, load_time, train_time
+    return output, load_time, inference_time
+
+def inference(
+    generator: Llama,
+    prompts: List[str],
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    max_gen_len: int = 64,
+):
+    results = generator.text_completion(
+        prompts,
+        max_gen_len=max_gen_len,
+        temperature=temperature,
+        top_p=top_p,
+    )
+    for prompt, result in zip(prompts, results):
+        print(prompt)
+        print(f"> {result['generation']}")
+        print("\n==================================\n")
 
 def __get_next_batch(dataloader):
     return next(iter(dataloader))
@@ -99,16 +142,16 @@ def benchmark():
     net = get_model()
 
     print("Running inference benchmark...\n")
-    _, load, inference, total = run_batch_inference(data_loader, net)
+    _, load, inference, total = run_benchmark(data_loader, net)
 
     print("Results...")
-    print("C2.1: Data-loading times")
+    print("Data-loading times")
     print("> per epoch: ", load)
     print("> average: ", torch.mean(load))
-    print("C2.2: Training time for each epoch")
+    print("\nInference time for each epoch")
     print("> per epoch", inference)
     print("> average", torch.mean(inference))
-    print("C2.3: Total time for each epoch")
+    print("\nTotal time for each epoch")
     print("> per epoch", total)
     print("> average", torch.mean(total))
 
