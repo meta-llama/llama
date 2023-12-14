84416a48c9b3575f48be8a79238434fec77e78c2
George Tamer <gtamer2@gmail.com>
2023-12-14 10:36:21 -0500

    revert

--- llama/__init__.py
+++ llama/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-from .generation_simplified import Llama, Dialog
+from .generation import Llama, Dialog
 from .model import ModelArgs, Transformer
 from .tokenizer import Tokenizer
