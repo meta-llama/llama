431ebab7b31c01232f4dc7e08ed8da9e2e6dac43
Guru Sharma <guru@gururams-air.lan>
2023-12-14 11:40:09 -0500

    added torch.jit.script to model in generation.py

--- llama/generation.py
+++ llama/generation.py
@@ -116,7 +116,7 @@ class Llama:
         tokenizer = Tokenizer(model_path=tokenizer_path)
         model_args.vocab_size = tokenizer.n_words
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
-        model = Transformer(model_args)
+        model = torch.jit.script(Transformer(model_args))
         model.load_state_dict(checkpoint, strict=False)
         print(f"Loaded in {time.time() - start_time:.2f} seconds")
 
