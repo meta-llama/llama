b93d20c7a4076b030afe2b02fddd5c897554ac23
George Tamer <gtamer2@gmail.com>
2023-12-15 16:06:43 -0500

    Fix logic

--- prune_model.py
+++ prune_model.py
@@ -30,10 +30,9 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
     return generator
 
 def calculate_model_sparsity(llama):
-    transformer = llama.model
     num_zeros = 0
     total_params = 0
-    for transformer_block in transformer.layers:
+    for transformer_block in llama.model.layers:
         all_layer_weights = torch.cat(
             (transformer_block.attention.wq.weight.flatten(), 
             transformer_block.attention.wk.weight.flatten(), 
@@ -42,18 +41,16 @@ def calculate_model_sparsity(llama):
             transformer_block.ffn_norm.weight.flatten()), 0)
 
         num_zeros += torch.sum(all_layer_weights == 0).item()
-        
         total_params += all_layer_weights.numel()
+        del all_layer_weights
         
     return num_zeros/total_params
 
 
 def prune_model(llama):
-    transformer = llama.model
+    print(f'model type = {type(llama.model)}')
     
-    print(f'model type = {type(transformer)}')
-    
-    for idx, transformer_block in enumerate(transformer.layers):
+    for idx, transformer_block in enumerate(llama.model.layers):
         check_mem()
         print(f'pruning layer {idx}')
         torch.cuda.empty_cache()
