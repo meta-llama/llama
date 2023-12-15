748bc32ac8e38b1e47674fc3acc5c70016b923ac
George Tamer <gtamer2@gmail.com>
2023-12-14 19:09:07 -0500

    Inplace

--- quantize_model.py
+++ quantize_model.py
@@ -23,19 +23,21 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
 def quantize_model(model):
     model.qconfig = torch.ao.quantization.default_qconfig
     torch.backends.quantized.engine = backend
-    model_static_quantized = torch.quantization.prepare(model, inplace=False)
-    model_static_quantized = torch.quantization.convert(model_static_quantized, inplace=False)
-    return model_static_quantized
+    torch.quantization.prepare(model, inplace=True)
+    torch.quantization.convert(model, inplace=True)
 
 
 def main():
     model = get_model("llama-2-7b/", "tokenizer.model", 512, 1)
-    q_model = quantize_model(model)
+    print("model size before in-place quantization")
     print_model_size(model)
-    print_model_size(q_model)
+
+    quantize_model(model)
+    print("model size after in-place quantization")
+    print_model_size(model)
 
     # save the quantized model
-    torch.save(q_model.state_dict(), "quantized_model.pt")
+    torch.save(model.state_dict(), "quantized_model.pt")
 
 if __name__ == "__main__":
     fire.Fire(main)
