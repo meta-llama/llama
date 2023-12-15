3c506ed8e188b81052ca2957ef5c2ce81222386c
George Tamer <gtamer2@gmail.com>
2023-12-15 15:00:52 -0500

    Debuggign pruning

--- prune_model.py
+++ prune_model.py
@@ -1,18 +1,8 @@
-import os
 import torch
 from llama import Llama
 import fire
-from gsmk_dataset import get_data_loader
 import torch
 import torch.nn.utils.prune as prune
-    
-backend = "qnnpack"
-
-def print_model_size(mdl):
-    torch.save(mdl.state_dict(), "tmp.pt")
-    print("%.2f MB" %(os.path.getsize("tmp.pt")/1e6))
-    os.remove('tmp.pt')
-
 
 
 def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
@@ -25,96 +15,49 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
     return generator
 
 def prune_model(llama):
-    model = llama.model
+    transformer = llama.model
     
-    print(f'model type = {type(model)}')
+    print(f'model type = {type(transformer)}')
     
     # set up pruning:
-    count = 0
     total_params = 0
     before_num_zeros = 0
     after_num_zeros = 0
-    for layer in model.layers: # each layer is a TransformerBlock
-        # we only have nn.Parameter objects in RMSNorm class
-        #import torch.nn.utils.prune as prune
+    for idx, transformer_block in enumerate(transformer.layers):
+        before_total_weights = transformer_block.attention.wq.weight + transformer_block.attention.wk.weight + transformer_block.attention.wv.weight + transformer_block.attention_norm.weight + transformer_block.ffn_norm.weight
+        
 
-        # Assuming you have a TransformerBlock object named 'layer'
-        #num_zeros = torch.sum(layer.weight == 0).item()
-        #total_params = layer.weight.numel()
-        #sparsity = num_zeros / total_params
-        #print(f"Sparsity of the TransformerBlock (before pruning): {sparsity}")
-        before_total_weights = layer.attention.wq.weight + layer.attention.wk.weight + layer.attention.wv.weight + layer.attention_norm.weight + layer.ffn_norm.weight
-        #att_norm_w = layer.attention_norm.weight
-        #ffn_norm_w = layer.ffn_norm.weight
-        #total_weights = attention_w + att_norm_w + ffn_norm_w
-        before_num_zeros += torch.sum(before_total_weights == 0).item()        
+        before_num_zeros += torch.sum(before_total_weights == 0).item()
+        
         total_params += before_total_weights.numel()
-        #print(f'Sparsity of the TransformerBlock (before pruning): {sparsity}')
         
-        
-        layer=prune.random_unstructured(layer, name="attn_norm_w", amount=0.3) # name is a torch.nn.Parameter
+        layer=prune.random_unstructured(layer, name="attn_norm_w", amount=0.3) # name has to be a torch.nn.Parameter
         
         after_total_weights = layer.attention.wq.weight + layer.attention.wk.weight + layer.attention.wv.weight + layer.attention_norm.weight + layer.ffn_norm.weight
-        after_num_zeros += torch.sum(after_total_weights == 0).item()        
         
-        #num_zeros = torch.sum(layer.weight == 0).item()
-        #sparsity = num_zeros / total_params
-        #print(f"Sparsity of the TransformerBlock (after pruning): {sparsity}")
-        # prune.l1_unstructured(layer, name="bias", amount=3)
-        count += 1
-        if count % 10**6 == 0:
-            print(f'we are {count} layers in')
-    
+        after_num_zeros += torch.sum(after_total_weights == 0).item()        
+
+        if idx % 10**6 == 0:
+            print(f'before_total_weights = {before_total_weights}')
+            print(f'before_num_zeros = {before_num_zeros}')
+            print(f'after_total_weights = {after_total_weights}')
+            print(f'after_num_zeros = {after_num_zeros}')
+            print(f'total_params = {total_params}')
+
+            print(f'we are {idx} layers in')
+
+    print(f'before_total_weights = {before_total_weights}')
+    print(f'before_num_zeros = {before_num_zeros}')
+    print(f'after_total_weights = {after_total_weights}')
+    print(f'after_num_zeros = {after_num_zeros}')
+    print(f'total_params = {total_params}')
+
     print(f'Sparsity of the TransformerBlock (before pruning): {before_num_zeros/total_params}')
     print(f'Sparsity of the TransformerBlock (after pruning): {after_num_zeros/total_params}')
-    
-    
-    
-    """for i in range(len(model.layers)): # each layer is a TransformerBlock
-        # we only have nn.Parameter objects in RMSNorm class
-        model.layers[i] = prune.random_unstructured(model.layers[i], name="attn_norm_w", amount=0.3) # name is a torch.nn.Parameter
-        # model.layer = prune.random_unstructured(layer, name="attn_norm_w", amount=0.3) # name is a torch.nn.Parameter
-        # prune.l1_unstructured(layer, name="bias", amount=3)
-    """
-        
-    
-    
-    #enc = model.encoder
-    #dec = model.decoder
-    
-    # setup quantization
-    """model.eval()
-    model.qconfig = torch.ao.quantization.get_default_qconfig('x86')
-    torch.backends.quantized.engine = backend
-    torch.quantization.prepare(model, inplace=True)
-    """
-    
-    # calibrate model to real world data
-    dataloader = get_data_loader(3, 0)
-    for _ in range(10):
-        batch = next(iter(dataloader))
-        llama.text_completion(
-            batch,
-            max_gen_len=512,
-            temperature=0.6,
-            top_p=0.9,
-        )
-
-    # convert in place
-    # torch.quantization.convert(model, inplace=True)
-
 
 def main():
     llama = get_model("/home/gyt2107/hpml_llama/llama-2-7b/", "tokenizer.model", 512, 6)
-    print("model size before in-place pruning")
-    print_model_size(llama.model)
-
     prune_model(llama)
-    print("model size after in-place pruning")
-    print_model_size(llama.model)
-
-    # save the quantized model
-    torch.save(llama.model.state_dict(), "quantized_model.pt")
 
 if __name__ == "__main__":
     fire.Fire(main)
