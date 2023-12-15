50771393c4289ba69276f3279835220a979f8836
George Tamer <gtamer2@gmail.com>
2023-12-15 15:27:34 -0500

    Fix logic

--- prune_model.py
+++ prune_model.py
@@ -14,20 +14,36 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
     )
     return generator
 
+def calculate_model_sparsity(llama):
+    transformer = llama.model
+    num_zeros = 0
+    total_params = 0
+    for transformer_block in transformer.layers:
+        # flatten all weights and append into one tensor
+        all_layer_weights = torch.cat(
+            (transformer_block.attention.wq.weight.flatten(), 
+            transformer_block.attention.wk.weight.flatten(), 
+            transformer_block.attention.wv.weight.flatten(), 
+            transformer_block.attention_norm.weight.flatten(), 
+            transformer_block.ffn_norm.weight.flatten()), 0)
+
+        num_zeros += torch.sum(all_layer_weights == 0).item()
+        
+        total_params += all_layer_weights.numel()
+        
+    return num_zeros/total_params
+
 def prune_model(llama):
     transformer = llama.model
     
     print(f'model type = {type(transformer)}')
     
     # set up pruning:
-    total_params = 0
-    before_num_zeros = 0
-    after_num_zeros = 0
     for idx, transformer_block in enumerate(transformer.layers):
-        print("shape of weights before pruning: ", transformer_block.attention.wq.weight.shape)
-        print("shape of weights before pruning: ", transformer_block.attention.wk.weight.shape)
-        print("shape of weights before pruning: ", transformer_block.attention.wv.weight.shape)
-        print("shape of weights before pruning: ", transformer_block.attention_norm.weight.shape)
+        # print("shape of weights before pruning: ", transformer_block.attention.wq.weight.shape)
+        # print("shape of weights before pruning: ", transformer_block.attention.wk.weight.shape)
+        # print("shape of weights before pruning: ", transformer_block.attention.wv.weight.shape)
+        # print("shape of weights before pruning: ", transformer_block.attention_norm.weight.shape)
 
         # flatten all weights and append into one tensor
         before_total_weights = torch.cat(
@@ -36,47 +52,52 @@ def prune_model(llama):
             transformer_block.attention.wv.weight.flatten(), 
             transformer_block.attention_norm.weight.flatten(), 
             transformer_block.ffn_norm.weight.flatten()), 0)
-        print("shape of weights before pruning: ", before_total_weights.shape)
+        # print("shape of weights before pruning: ", before_total_weights.shape)
 
-        before_num_zeros += torch.sum(before_total_weights == 0).item()
+        # before_num_zeros += torch.sum(before_total_weights == 0).item()
         
-        total_params += before_total_weights.numel()
+        # total_params += before_total_weights.numel()
         
-        layer=prune.random_unstructured(transformer_block, name="attn_norm_w", amount=0.3) # name has to be a torch.nn.Parameter
-        layer=prune.random_unstructured(transformer_block.attention.wq, name="weight", amount=0.3)
-        layer=prune.random_unstructured(transformer_block.attention.wk, name="weight", amount=0.3)
-        layer=prune.random_unstructured(transformer_block.attention.wv, name="weight", amount=0.3)
-        layer=prune.random_unstructured(transformer_block.attention.wo, name="weight", amount=0.3)
+        prune.random_unstructured(transformer_block, name="attn_norm_w", amount=0.3) # name has to be a torch.nn.Parameter
+        prune.random_unstructured(transformer_block.attention.wq, name="weight", amount=0.3)
+        prune.random_unstructured(transformer_block.attention.wk, name="weight", amount=0.3)
+        prune.random_unstructured(transformer_block.attention.wv, name="weight", amount=0.3)
+        prune.random_unstructured(transformer_block.attention.wo, name="weight", amount=0.3)
         
         # flatten all weights and append into one tensor
-        after_total_weights = torch.cat(
-            (layer.attention.wq.weight.flatten(), 
-            layer.attention.wk.weight.flatten(), 
-            layer.attention.wv.weight.flatten(), 
-            layer.attention_norm.weight.flatten(), 
-            layer.ffn_norm.weight.flatten()), 0)
-        after_num_zeros += torch.sum(after_total_weights == 0).item()        
+        # after_total_weights = torch.cat(
+        #     (layer.attention.wq.weight.flatten(), 
+        #     layer.attention.wk.weight.flatten(), 
+        #     layer.attention.wv.weight.flatten(), 
+        #     layer.attention_norm.weight.flatten(), 
+        #     layer.ffn_norm.weight.flatten()), 0)
+        # after_num_zeros += torch.sum(after_total_weights == 0).item()        
 
-        if idx % 10**2 == 0:
-            print(f'before_total_weights = {before_total_weights}')
-            print(f'before_num_zeros = {before_num_zeros}')
-            print(f'after_total_weights = {after_total_weights}')
-            print(f'after_num_zeros = {after_num_zeros}')
-            print(f'total_params = {total_params}')
-            print(f'we are {idx} layers in')
+    #     if idx % 10**2 == 0:
+    #         print(f'before_total_weights = {before_total_weights}')
+    #         print(f'before_num_zeros = {before_num_zeros}')
+    #         print(f'after_total_weights = {after_total_weights}')
+    #         print(f'after_num_zeros = {after_num_zeros}')
+    #         print(f'total_params = {total_params}')
+    #         print(f'we are {idx} layers in')
 
-    print(f'before_total_weights = {before_total_weights}')
-    print(f'before_num_zeros = {before_num_zeros}')
-    print(f'after_total_weights = {after_total_weights}')
-    print(f'after_num_zeros = {after_num_zeros}')
-    print(f'total_params = {total_params}')
+    # print(f'before_total_weights = {before_total_weights}')
+    # print(f'before_num_zeros = {before_num_zeros}')
+    # print(f'after_total_weights = {after_total_weights}')
+    # print(f'after_num_zeros = {after_num_zeros}')
+    # print(f'total_params = {total_params}')
 
-    print(f'Sparsity of the TransformerBlock (before pruning): {before_num_zeros/total_params}')
-    print(f'Sparsity of the TransformerBlock (after pruning): {after_num_zeros/total_params}')
+    # print(f'Sparsity of the TransformerBlock (before pruning): {before_num_zeros/total_params}')
+    # print(f'Sparsity of the TransformerBlock (after pruning): {after_num_zeros/total_params}')
 
 def main():
     llama = get_model("/home/gyt2107/hpml_llama/llama-2-7b/", "tokenizer.model", 512, 6)
+    init_sparsity = calculate_model_sparsity(llama)
     prune_model(llama)
+    final_sparsity = calculate_model_sparsity(llama)
+
+    print(f'init_sparsity = {init_sparsity}')
+    print(f'final_sparsity = {final_sparsity}')
 
 if __name__ == "__main__":
     fire.Fire(main)
