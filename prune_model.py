1a458c2811eb4ff558ddb290ca4b79bd371e8347
George Tamer <gtamer2@gmail.com>
2023-12-16 11:19:07 -0500

    Prune model

--- prune_model.py
+++ prune_model.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn.utils.prune as prune
 import sys
 import nvidia_smi
+import os
 
 def check_mem():
     nvidia_smi.nvmlInit()
@@ -20,6 +21,7 @@ def check_mem():
 
     nvidia_smi.nvmlShutdown()
 
+
 def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
@@ -28,6 +30,7 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
         max_batch_size=max_batch_size,
     )
     return generator
+
 
 def calculate_model_sparsity(llama):
     num_zeros = 0
@@ -50,7 +53,7 @@ def calculate_model_sparsity(llama):
 def prune_model(llama):
     print(f'model type = {type(llama.model)}')
     print(f'model layers = {len(llama.model.layers)}')
-    
+
     for idx, transformer_block in enumerate(llama.model.layers):
         check_mem()
         print(f'pruning layer {idx}')
@@ -68,64 +71,56 @@ def prune_model(llama):
         prune.random_unstructured(transformer_block.attention.wo, name="weight", amount=0.3)        
         torch.cuda.empty_cache()
 
-        # prune_model_inner(transformer_block, name="attn_norm_w", amount=0.3)
-        # prune_model_inner(transformer_block.attention.wq, name="weight", amount=0.3)
-        # prune_model_inner(transformer_block.attention.wk, name="weight", amount=0.3)
-        # prune_model_inner(transformer_block.attention.wv, name="weight", amount=0.3)
-        # prune_model_inner(transformer_block.attention.wo, name="weight", amount=0.3)
 
-def prune_model_all(llama, start_ind):
+def prune_model_layer(llama, layer_idx):
     print(f'model type = {type(llama.model)}')
     print(f'model layers = {len(llama.model.layers)}')
-    
-    for idx, transformer_block in enumerate(llama.model.layers[start_ind:start_ind+2]):
-        check_mem()
-        print(f'pruning layer {idx}')
-        torch.cuda.empty_cache()
-        if idx > 1:
-            break
-        prune.random_unstructured(transformer_block, name="attn_norm_w", amount=0.3) # name has to be a torch.nn.Parameter
-        torch.cuda.empty_cache()
-        prune.random_unstructured(transformer_block.attention.wq, name="weight", amount=0.3)
-        torch.cuda.empty_cache()
-        prune.random_unstructured(transformer_block.attention.wk, name="weight", amount=0.3)
-        torch.cuda.empty_cache()
-        prune.random_unstructured(transformer_block.attention.wv, name="weight", amount=0.3)
-        torch.cuda.empty_cache()
-        prune.random_unstructured(transformer_block.attention.wo, name="weight", amount=0.3)        
-        torch.cuda.empty_cache()
+    transformer_block = llama.model.layers[layer_idx]
+    check_mem()
+    print(f'pruning layer {layer_idx}')
+    torch.cuda.empty_cache()
+    prune.random_unstructured(transformer_block, name="attn_norm_w", amount=0.3) # name has to be a torch.nn.Parameter
+    torch.cuda.empty_cache()
+    prune.random_unstructured(transformer_block.attention.wq, name="weight", amount=0.3)
+    torch.cuda.empty_cache()
+    prune.random_unstructured(transformer_block.attention.wk, name="weight", amount=0.3)
+    torch.cuda.empty_cache()
+    prune.random_unstructured(transformer_block.attention.wv, name="weight", amount=0.3)
+    torch.cuda.empty_cache()
+    prune.random_unstructured(transformer_block.attention.wo, name="weight", amount=0.3)        
+    torch.cuda.empty_cache()
 
-
-
-def prune_model_inner(module, name, amount):
-    prune.random_unstructured(module, name=name, amount=amount)
 
 def main():
-    print(f'First argument = {sys.argv[1]}')
+    layer = int(sys.argv[1])
     print("Starting up...")
-    llama = get_model("/home/gyt2107/hpml_llama/llama-2-7b/", "to_mod_tokenizer.pt", 512, 4)
+
+    checkpoint_dir = "/home/gyt2107/hpml_llama/llama-2-7b-pruned/"
+    llama = get_model(checkpoint_dir, "tokenizer.model", 512, 4)
     check_mem()
     print("Model loaded")
+
     print("Calculating sparsity...")
     init_sparsity = calculate_model_sparsity(llama)
-    torch.cuda.empty_cache()
-    check_mem()
     print(f'init_sparsity = {init_sparsity}')
     
-    
-    print("Pruning model...")
-    #prune_model(llama)
-    prune_model_all(llama, 2*int(sys.argv[1])) # go from 0 to 15
-    print("Pruning done")
     torch.cuda.empty_cache()
-    
-    
     check_mem()
+    
+    print(f"Pruning model layer {layer}...")
+    prune_model_layer(llama, layer)
+    print("Pruning done")
+
+    torch.cuda.empty_cache()
+    check_mem()
+
     print("Calculating sparsity...")
     final_sparsity = calculate_model_sparsity(llama)
     print(f'final_sparsity = {final_sparsity}')
-    #print(f'Last argument = {sys.argv[2]}')
-    torch.save(llama.model.state_dict(), "to_mod_tokenizer.pt")
+
+    print("Saving model...")
+    save_path = os.path.join(checkpoint_dir, f"consolidated.00.pth")
+    torch.save(llama.model.state_dict(), save_path)
 
 if __name__ == "__main__":
     fire.Fire(main)
