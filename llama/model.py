1e8375848d3a3ebaccab83fd670b880864cf9409
wangzhihong <wangzhihong@cmss.chinamobile.com>
2024-03-21 10:09:34 +0800

    update the code to use the module's __call__

--- llama/model.py
+++ llama/model.py
@@ -403,10 +403,10 @@ class TransformerBlock(nn.Module):
             torch.Tensor: Output tensor after applying attention and feedforward layers.
 
         """
-        h = x + self.attention.forward(
+        h = x + self.attention(
             self.attention_norm(x), start_pos, freqs_cis, mask
         )
-        out = h + self.feed_forward.forward(self.ffn_norm(h))
+        out = h + self.feed_forward(self.ffn_norm(h))
         return out
 
 
