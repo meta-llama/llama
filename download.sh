ee2e4231b75ce5fb89ba614e603b40e1ec789edd
TrentStew <168601471+TrentStew@users.noreply.github.com>
2024-04-30 19:17:36 -0400

    Update SH

--- download.sh
+++ download.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 set -e
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "Enter the URL from email: " https://download6.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoiN2sxZHY1bm16d3BtemczaXB1aXpxcmk4IiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQ2LmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTQ1OTk4NTV9fX1dfQ__&Signature=KF6zW8w6ya-7zCe7yAFgCmdZgl8WwNfpQNOcnlNS7JTLBdVZxm3NTdlmSExYs-N0v9ugOKQQZ8SX7lQ2BaTQBaVaxA2QjWTMKo3sp61sFUN70vqGtxTfn14dMkjK%7Eck0TizYDe8MU3-VDcRzrTiMtM%7EaosLTRV9557ygYjFeUUPHQY4iDKPnKNqnWgn-caZJUXCYCPKBcdjxOCqaKi%7EDJFTEvsKB8Yta6jpEqZ1njjpY61UXrtUG5P-h3DA3l3s60ttDudptJbzcPmzcgYpRZM4KV1pwtJFBr4HaX%7ELKjEOB8ytrvZXhEW40XvFCp4LPgZ34savCaltlWtuvD7D0SQ__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=1152314035795804
 echo ""
-read -p "Enter the list of models to download without spaces (7B,13B,70B,7B-chat,13B-chat,70B-chat), or press Enter for all: " MODEL_SIZE
+read -p "Enter the list of models to download without spaces (7B,13B,70B,7B-chat,13B-chat,70B-chat), or press Enter for all: " 7b-chat
 TARGET_FOLDER="."             # where all files should end up
 mkdir -p ${TARGET_FOLDER}
 
